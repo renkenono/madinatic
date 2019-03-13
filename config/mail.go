a8d76d7ed165841bfcd98437a7ec314243b4fa41
Renken <renken@verm.im>
2019-03-13 15:57:18 +0100

    add mail sending feature

---
+++ config/mail.go
@@ -0,0 +1,41 @@
+package config
+
+import (
+	"errors"
+	"net"
+	"net/smtp"
+)
+
+// Mail represents plantext mail net/smtp
+// for this software, `to` will only contain one email
+// from is given in config
+// TODO: add HTML mail
+type Mail struct {
+	to      string
+	subject string
+	body    string
+}
+
+// New returns a Mail struct
+func New(to string, subj string, body string) Mail {
+	return Mail{
+		to:      to,
+		subject: subj,
+		body:    body,
+	}
+}
+
+// Send prepares msg body for net/SendMail
+func (m *Mail) Send() error {
+	host, _, err := net.SplitHostPort(App.ESrv)
+	if err != nil {
+		return errors.New("send: splithostport failed: " + err.Error())
+	}
+	auth := smtp.PlainAuth(NAME, App.EMail, App.EPass, host)
+	msg := []byte("To: " + m.to + "\r\nSubject: " + m.subject + "\r\n" + m.body)
+	err = smtp.SendMail(App.ESrv, auth, App.EMail, []string{m.to}, msg)
+	if err != nil {
+		return errors.New("send: sendmail: " + err.Error())
+	}
+	return nil
+}
