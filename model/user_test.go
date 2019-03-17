2cf93e94592c00d8c3a063f798911c94f6319f52
Renken <renken@verm.im>
2019-03-17 14:29:10 +0100

    add check for existing fields tests, update other threads

--- model/user_test.go
+++ model/user_test.go
@@ -1,10 +1,25 @@
 package model
 
-import "testing"
+import (
+	"testing"
 
+	_ "github.com/go-sql-driver/mysql"
+	"github.com/renkenn/madinatic/config"
+)
+
+func TestValidateUserID(t *testing.T) {
+	for _, id := range []string{"0", "32142142155151515151515", "213213214", "109891379001120119", "999999999999999999", "9999999999999999999", "h"} {
+		nid, err := ValidateUserID(id)
+		t.Log(id)
+		if err != nil {
+			t.Error(err)
+		}
+		t.Log(nid)
+	}
+}
 func TestValidateUsername(t *testing.T) {
 	for _, user := range []string{"renken", "renken13", "ren-chan", "_renchan", "1renken", "renkenennenenenenennenenenennenenennenenen"} {
-		err := ValidateUsername(user)
+		_, err := ValidateUsername(user)
 		t.Log(user)
 		if err != nil {
 			t.Error(err)
@@ -14,7 +29,7 @@ func TestValidateUsername(t *testing.T) {
 
 func TestValidateEmail(t *testing.T) {
 	for _, email := range []string{"renken@siga.dz", "_renken@si ga.dz"} {
-		err := ValidateEmail(email)
+		_, err := ValidateEmail(email)
 		t.Log(email)
 		if err != nil {
 			t.Error(err)
@@ -24,7 +39,7 @@ func TestValidateEmail(t *testing.T) {
 
 func TestValidatePass(t *testing.T) {
 	for _, pass := range []string{"spaces are valid", "length", "\u0000controlcodepoint", "bobandalicédon'texist"} {
-		err := ValidatePass(pass)
+		_, err := ValidatePass(pass)
 		t.Log("password: " + pass)
 		if err != nil {
 			t.Error(err)
@@ -33,9 +48,90 @@ func TestValidatePass(t *testing.T) {
 }
 
 func TestValidatePhone(t *testing.T) {
-	for _, p := range []string{"+213555555555", "+231555555555", "+213455555555", "+21374545"} {
-		err := ValidatePhone(p)
+	for _, p := range []string{"213555555555", "231555555555", "213455555555", "21374545"} {
+		np, err := ValidatePhone(p)
 		t.Log("phone: " + p)
+		t.Log(np)
+		if err != nil {
+			t.Error(err)
+		}
+	}
+}
+
+func TestExistsUserID(t *testing.T) {
+	dsn, err := config.App.LoadConfig("../config.json")
+	if err != nil {
+		t.Error(err)
+	}
+
+	err = config.DB.InitDB(dsn)
+	if err != nil {
+		t.Error(err)
+	}
+
+	for _, id := range []string{"101", "102"} {
+		err := ExistsUserID(id)
+		t.Log(id)
+		if err != nil {
+			t.Error(err)
+		}
+	}
+}
+
+func TestExistsUsername(t *testing.T) {
+	dsn, err := config.App.LoadConfig("../config.json")
+	if err != nil {
+		t.Error(err)
+	}
+
+	err = config.DB.InitDB(dsn)
+	if err != nil {
+		t.Error(err)
+	}
+
+	for _, name := range []string{"renkenn", "admin"} {
+		err := ExistsUsername(name)
+		t.Log(name)
+		if err != nil {
+			t.Error(err)
+		}
+	}
+}
+
+func TestExistsPhone(t *testing.T) {
+	dsn, err := config.App.LoadConfig("../config.json")
+	if err != nil {
+		t.Error(err)
+	}
+
+	err = config.DB.InitDB(dsn)
+	if err != nil {
+		t.Error(err)
+	}
+
+	for _, phone := range []string{"213555555555", "72324324"} {
+		err := ExistsPhone(phone)
+		t.Log(phone)
+		if err != nil {
+			t.Error(err)
+		}
+	}
+}
+
+func TestExistsEmail(t *testing.T) {
+	dsn, err := config.App.LoadConfig("../config.json")
+	if err != nil {
+		t.Error(err)
+	}
+
+	err = config.DB.InitDB(dsn)
+	if err != nil {
+		t.Error(err)
+	}
+
+	for _, email := range []string{"renken@email.com", "renken@siga.dz"} {
+		err := ExistsEmail(email)
+		t.Log(email)
 		if err != nil {
 			t.Error(err)
 		}
