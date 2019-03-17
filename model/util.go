b729627289bdb505650bb553c6cc6294eabe916a
Renken <renken@verm.im>
2019-03-17 23:57:28 +0100

    add bcrypt helper functions

---
+++ model/util.go
@@ -0,0 +1,15 @@
+package model
+
+import "golang.org/x/crypto/bcrypt"
+
+// HashPassword is a helper function to hash passwords
+func HashPassword(password string) (string, error) {
+	bytes, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
+	return string(bytes), err
+}
+
+// CheckPasswordHash helper function to compare passwords hashes
+func CheckPasswordHash(password, hash string) error {
+	err := bcrypt.CompareHashAndPassword([]byte(hash), []byte(password))
+	return err
+}
