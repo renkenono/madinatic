1307db4207bdd57087ad4b3111b865e4d4a3f18d
Renken <renken@verm.im>
2019-03-17 23:51:24 +0100

    add tests for user functions

--- model/user_test.go
+++ model/user_test.go
@@ -1,7 +1,10 @@
 package model
 
 import (
+	"fmt"
 	"testing"
+
+	"golang.org/x/crypto/bcrypt"
 
 	_ "github.com/go-sql-driver/mysql"
 	"github.com/renkenn/madinatic/config"
@@ -70,7 +73,7 @@ func TestExistsUserID(t *testing.T) {
 	}
 
 	for _, id := range []string{"101", "102"} {
-		err := ExistsUserID(id)
+		_, err := ExistsUserID(id)
 		t.Log(id)
 		if err != nil {
 			t.Error(err)
@@ -89,8 +92,8 @@ func TestExistsUsername(t *testing.T) {
 		t.Error(err)
 	}
 
-	for _, name := range []string{"renkenn", "admin"} {
-		err := ExistsUsername(name)
+	for _, name := range []string{"renkenn", "admin", "renken"} {
+		_, err := ExistsUsername(name)
 		t.Log(name)
 		if err != nil {
 			t.Error(err)
@@ -110,8 +113,9 @@ func TestExistsPhone(t *testing.T) {
 	}
 
 	for _, phone := range []string{"213555555555", "72324324"} {
-		err := ExistsPhone(phone)
+		np, err := ExistsPhone(phone)
 		t.Log(phone)
+		t.Log(np)
 		if err != nil {
 			t.Error(err)
 		}
@@ -130,10 +134,256 @@ func TestExistsEmail(t *testing.T) {
 	}
 
 	for _, email := range []string{"renken@email.com", "renken@siga.dz"} {
-		err := ExistsEmail(email)
+		_, err := ExistsEmail(email)
 		t.Log(email)
 		if err != nil {
 			t.Error(err)
 		}
+	}
+}
+
+func TestNewUser(t *testing.T) {
+	dsn, err := config.App.LoadConfig("../config.json")
+	if err != nil {
+		t.Error(err)
+	}
+
+	err = config.DB.InitDB(dsn)
+	if err != nil {
+		t.Error(err)
+	}
+
+	u, err := NewUser("109891379001120119", "renken", "renken@tfwno.gf", "renkenpass", "213555555555", true)
+	if err != nil {
+		t.Error(err)
+	} else {
+		t.Log(u)
+	}
+}
+
+func TestUserByID(t *testing.T) {
+	dsn, err := config.App.LoadConfig("../config.json")
+	if err != nil {
+		t.Error(err)
+	}
+
+	err = config.DB.InitDB(dsn)
+	if err != nil {
+		t.Error(err)
+	}
+
+	u, err := UserByID("109891379001120119")
+	if err != nil {
+		t.Error(err)
+	} else {
+		t.Log(u)
+	}
+}
+
+func TestUserByUsername(t *testing.T) {
+	dsn, err := config.App.LoadConfig("../config.json")
+	if err != nil {
+		t.Error(err)
+	}
+
+	err = config.DB.InitDB(dsn)
+	if err != nil {
+		t.Error(err)
+	}
+
+	u, err := UserByUsername("renken")
+	if err != nil {
+		t.Error(err)
+	} else {
+		t.Error(u)
+	}
+}
+
+func TestEditEmail(t *testing.T) {
+	dsn, err := config.App.LoadConfig("../config.json")
+	if err != nil {
+		t.Error(err)
+	}
+
+	err = config.DB.InitDB(dsn)
+	if err != nil {
+		t.Error(err)
+	}
+
+	u, err := UserByID("109891379001120119")
+	if err != nil {
+		t.Error(err)
+	} else {
+		t.Log(u)
+	}
+	err = u.EditEmail("renken@gmail.com")
+	if err != nil {
+		t.Error(err)
+	}
+	fmt.Println(u)
+}
+
+func TestEditPhone(t *testing.T) {
+	dsn, err := config.App.LoadConfig("../config.json")
+	if err != nil {
+		t.Error(err)
+	}
+
+	err = config.DB.InitDB(dsn)
+	if err != nil {
+		t.Error(err)
+	}
+
+	u, err := UserByID("109891379001120119")
+	if err != nil {
+		t.Error(err)
+	} else {
+		t.Log(u)
+	}
+	err = u.EditPhone("213666162318")
+	if err != nil {
+		t.Error(err)
+	}
+}
+
+func TestEditPass(t *testing.T) {
+	dsn, err := config.App.LoadConfig("../config.json")
+	if err != nil {
+		t.Error(err)
+	}
+
+	err = config.DB.InitDB(dsn)
+	if err != nil {
+		t.Error(err)
+	}
+
+	u, err := UserByID("109891379001120119")
+	if err != nil {
+		t.Error(err)
+	} else {
+		t.Log(u)
+	}
+	err = u.EditPass("secret_pass_changed")
+	if err != nil {
+		t.Error(err)
+	}
+}
+
+func TestDelete(t *testing.T) {
+	dsn, err := config.App.LoadConfig("../config.json")
+	if err != nil {
+		t.Error(err)
+	}
+
+	err = config.DB.InitDB(dsn)
+	if err != nil {
+		t.Error(err)
+	}
+
+	u, err := UserByID("109891379001120119")
+	if err != nil {
+		t.Error(err)
+	} else {
+		t.Log(u)
+	}
+	err = u.Delete()
+	if err != nil {
+		t.Error(err)
+	}
+}
+
+func TestConfirmed(t *testing.T) {
+	dsn, err := config.App.LoadConfig("../config.json")
+	if err != nil {
+		t.Error(err)
+	}
+
+	err = config.DB.InitDB(dsn)
+	if err != nil {
+		t.Error(err)
+	}
+
+	u, err := UserByID("109891379001120119")
+	if err != nil {
+		t.Error(err)
+	} else {
+		t.Log(u)
+	}
+	err = u.Confirmed()
+	if err != nil {
+		t.Error(err)
+	}
+}
+
+func TestConfirm(t *testing.T) {
+	dsn, err := config.App.LoadConfig("../config.json")
+	if err != nil {
+		t.Error(err)
+	}
+
+	err = config.DB.InitDB(dsn)
+	if err != nil {
+		t.Error(err)
+	}
+
+	u, err := UserByID("109891379001120119")
+	if err != nil {
+		t.Error(err)
+	} else {
+		t.Log(u)
+	}
+	err = u.Confirmed()
+	if err != nil {
+		if err == ErrUserNotConfirmed {
+			err := u.Confirm()
+			if err != nil {
+				t.Error(err)
+			}
+		}
+		t.Error(err)
+	}
+}
+
+func TestUsers(t *testing.T) {
+	dsn, err := config.App.LoadConfig("../config.json")
+	if err != nil {
+		t.Error(err)
+	}
+
+	err = config.DB.InitDB(dsn)
+	if err != nil {
+		t.Error(err)
+	}
+
+	users, err := Users()
+	if err != nil {
+		t.Error(err)
+	}
+
+	for _, u := range users {
+		t.Log(u)
+	}
+	t.Error("success")
+}
+
+func TestLogin(t *testing.T) {
+	dsn, err := config.App.LoadConfig("../config.json")
+	if err != nil {
+		t.Error(err)
+	}
+
+	err = config.DB.InitDB(dsn)
+	if err != nil {
+		t.Error(err)
+	}
+
+	u, err := Login("renken", "renkenpassfaulty")
+	if err != nil {
+		if err == bcrypt.ErrMismatchedHashAndPassword {
+			t.Error("Login failed")
+		}
+		t.Error(err)
+	} else {
+		t.Error(u)
 	}
 }
