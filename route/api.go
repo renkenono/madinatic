08c7aef20de90a650e893f922eb3e4c1967cfbd6
Renken <renken@verm.im>
2019-03-20 19:10:52 +0100

    add /api/login route handler

--- route/api.go
+++ route/api.go
@@ -2,9 +2,11 @@ package route
 
 import (
 	"github.com/gorilla/mux"
+	"github.com/renkenn/madinatic/control/user"
 )
 
 // APIRoutes assings API handlers to a given mux
-func APIRoutes(mux *mux.Router) {
-	//
+func APIRoutes(r *mux.Router) {
+	api := r.PathPrefix("/api").Subrouter()
+	api.HandleFunc("/login", user.LoginAPI)
 }
