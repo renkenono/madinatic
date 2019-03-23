0d16d240e330199d4e27aa64c52a11b0e23a172b
Renken <renken@verm.im>
2019-03-23 22:14:23 +0100

    add helper functions and error messages

--- control/util.go
+++ control/util.go
@@ -1,6 +1,7 @@
 package control
 
 import (
+	"encoding/json"
 	"html/template"
 	"log"
 	"net/http"
@@ -10,11 +11,29 @@ import (
 )
 
 const (
-	OutUserDoesNotExist = 0
-	OutWrongPassword    = iota
-	OutUserNotConfirmed = iota
-	ViewAuth            = 0
-	ViewHomePage        = iota
+	ExpireTime = 5097600
+
+	ErrUserDoesNotExist  = 1
+	ErrWrongPassword     = iota
+	ErrUserNotConfirmed  = iota
+	ErrUserIDInvalid     = iota
+	ErrUserIDExists      = iota
+	ErrUsernameInvalid   = iota
+	ErrUsernameExists    = iota
+	ErrEmailInvalid      = iota
+	ErrEmailExists       = iota
+	ErrPhoneInvalid      = iota
+	ErrPhoneExists       = iota
+	ErrPassInvalid       = iota
+	ErrRePassInvalid     = iota
+	ErrFirstNameInvalid  = iota
+	ErrFamilyNameInvalid = iota
+)
+
+const (
+	ViewLogin    = 0
+	ViewRegister = iota
+	ViewHomePage = iota
 )
 
 // Out holds messages sent to the end user
@@ -24,13 +43,27 @@ const (
 // and Out["fr"][OutUserNotConfirmed]
 var (
 	Out = []string{
+		"",
 		"Utilisateur n'existe pas",
 		"Mot de passe incorrecte",
 		"Utilisateur n'est pas confirmé",
+		"ID est invalid",
+		"ID existe déja",
+		"Username est invalid",
+		"Username existe déja",
+		"Email est invalid",
+		"Email existe déja",
+		"Phone est invalid",
+		"Phone existe déja",
+		"Mot de passe est invalid",
+		"Mot de passe n'est pas le même",
+		"Prénom est invalid",
+		"Nom est invalid",
 	}
 
 	View = []string{
-		"auth",
+		"login",
+		"register",
 	}
 
 	viewsPath = path.Join("web", "views")
@@ -45,9 +78,31 @@ func Render(w http.ResponseWriter, r *http.Request, data interface{}, v int, fns
 	tmpl = template.Must(template.ParseFiles(tmpls...))
 
 	// map[string]interface{} {} for readability
-	err := tmpl.ExecuteTemplate(w, "auth", data)
+	err := tmpl.ExecuteTemplate(w, View[v], data)
 	if err != nil {
 		log.Fatalf("%stemplate error: %s", config.FATAL, err.Error())
 	}
 
+}
+
+func DecodeJSON(w http.ResponseWriter, r *http.Request, data interface{}, suff string) error {
+	err := json.NewDecoder(r.Body).Decode(data)
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		log.Printf("%s%s: %s", config.ERROR, suff, err.Error())
+		return err
+	}
+	return nil
+}
+
+func MarshalJSON(w http.ResponseWriter, data interface{}, suff string) {
+	djson, err := json.Marshal(data)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		log.Printf("%s%s: %s", config.FATAL, suff, err.Error())
+		return
+	}
+
+	w.WriteHeader(http.StatusOK)
+	w.Write(djson)
 }
