fba1a70640a6c84a4fa91efe70c12466f10b2632
Renken <renken@verm.im>
2019-09-13 15:00:45 +0100

    broken session manager, fix later

--- control/user/home.go
+++ control/user/home.go
@@ -10,6 +10,7 @@ import (
 	. "github.com/renkenn/madinatic/control"
 )
 
+// Home page
 func Home(w http.ResponseWriter, r *http.Request) {
 	s, err := config.Store.Get(r, "userdata")
 	if err != nil {
@@ -19,7 +20,8 @@ func Home(w http.ResponseWriter, r *http.Request) {
 	}
 
 	fstr := "welcome guest."
-	if !s.IsNew {
+	_, ok := s.Values["username"]
+	if !s.IsNew && !ok {
 		// render guest homepage
 
 		fstr = "welcome " + s.Values["username"].(string)
