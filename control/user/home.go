397cc16c2c7932de6812148fd106b09b65b1dba0
Renken <renken@verm.im>
2019-09-17 21:12:14 +0100

    add faq, err and update home

--- control/user/home.go
+++ control/user/home.go
@@ -1,8 +1,11 @@
 package user
 
 import (
+	"fmt"
 	"log"
 	"net/http"
+
+	"github.com/renkenn/madinatic/model"
 
 	"github.com/gorilla/csrf"
 
@@ -10,31 +13,114 @@ import (
 	. "github.com/renkenn/madinatic/control"
 )
 
+type reportSlide struct {
+	Title      string
+	Address    string
+	Categories []string
+	Picture    string
+	Link       string
+}
+
+const (
+	homeViewErr = "homepage view failed"
+	faqViewErr  = "faq view failed"
+)
+
 // Home page
 func Home(w http.ResponseWriter, r *http.Request) {
 	s, err := config.Store.Get(r, "userdata")
 	if err != nil {
 		http.Redirect(w, r, "/error", http.StatusInternalServerError)
-		log.Printf("%s%s: %s", config.INFO, logErr, err.Error())
+		log.Printf("%s%s: %s", config.INFO, homeViewErr, err.Error())
 		return
 	}
 
-	fstr := "welcome guest."
+	guest := false
 	_, ok := s.Values["username"]
-	if !s.IsNew && !ok {
+	if s.IsNew || !ok {
 		// render guest homepage
 
-		fstr = "welcome " + s.Values["username"].(string)
-		// fstr += s.Flashes()[0].(string)
+		guest = true
 	}
 
 	// render user homepage
+	rs, err := model.ReportsLatest(4)
+	if err != nil {
+		http.Redirect(w, r, "/error", http.StatusInternalServerError)
+		log.Printf("%s%s: %s", config.INFO, homeViewErr, err.Error())
+		return
+	}
+
+	var rsslides [][]reportSlide
+	j := 0
+	var slide []reportSlide
+	for i, re := range rs {
+		pics, err := re.Pics()
+		if err != nil {
+			http.Redirect(w, r, "/error", http.StatusInternalServerError)
+			log.Printf("%s%s: %s", config.INFO, homeViewErr, err.Error())
+			return
+		}
+
+		cats, err := re.Categories()
+		if err != nil {
+			http.Redirect(w, r, "/error", http.StatusInternalServerError)
+			log.Printf("%s%s: %s", config.INFO, homeViewErr, err.Error())
+			return
+		}
+
+		slide = append(slide, reportSlide{
+			Title:      re.Title,
+			Address:    re.Address,
+			Categories: cats,
+			Picture:    pics[0],
+			Link:       fmt.Sprintf("/report/view/%d", re.ID),
+		})
+		if i%2 == 1 {
+			rsslides = append(rsslides, slide)
+			j++
+		}
+	}
 
 	data := map[string]interface{}{
 		"csrfField": csrf.TemplateField(r),
-		"flash":     fstr,
+		"guest":     guest,
+		"slides":    rsslides,
 	}
 
 	Render(w, r, data, ViewHome, "home.tmpl")
+
+}
+
+// FAQ page
+func FAQ(w http.ResponseWriter, r *http.Request) {
+	s, err := config.Store.Get(r, "userdata")
+	if err != nil {
+		http.Redirect(w, r, "/error", http.StatusInternalServerError)
+		log.Printf("%s%s: %s", config.INFO, faqViewErr, err.Error())
+		return
+	}
+
+	guest := false
+	_, ok := s.Values["username"]
+
+	if s.IsNew || !ok {
+		// render guest homepage
+		guest = true
+	}
+
+	data := map[string]interface{}{
+		"csrfField": csrf.TemplateField(r),
+		"guest":     guest,
+	}
+
+	Render(w, r, data, ViewFAQ, "faq.tmpl")
+
+}
+
+// Err page
+func Err(w http.ResponseWriter, r *http.Request) {
+
+	Render(w, r, nil, ViewErr, "err.tmpl")
 
 }
